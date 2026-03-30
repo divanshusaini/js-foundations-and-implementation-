2c75623e890a8878f40b93d86dcf46338f78af67
divashusaini <dishusaini2july@gmail.com>
2026-03-30 14:56:57 +0530

    promise chaining

--- promises/index.js
+++ promises/index.js
@@ -1,16 +1,27 @@
-//Promises
+// //Promises
 
 
-console.log("hello promises");
+// console.log("hello promises");
 
-const result= fetch("https://api.github.com/users");
+// const result= fetch("https://api.github.com/users");
 
-const ans = result.then((Response)=>{
-   return  Response.json();
+// const ans = result.then((Response)=>{
+//    return  Response.json();
     
-});
-ans.then((Response)=>{
-console.log(Response);
-})
+// });
+// ans.then((Response)=>{
+// console.log(Response);
+// })
+ 
 
+//Promise chaining
+
+fetch("https://api.github.com/users")
+.then((response)=>{
+       return response.json();
+})
+.then((data)=>{
+ console.log(data);
+ 
+});
 
